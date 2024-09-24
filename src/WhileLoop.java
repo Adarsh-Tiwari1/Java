7e27a6b827e453b969b82101f1b2d3cb8472108a
Adarsh Tiwari <153628277+Adarsh-Tiwari1@users.noreply.github.com>
2024-09-24 15:15:41 +0530

    :WhileLoop

---
+++ src/WhileLoop.java
@@ -0,0 +1,56 @@
+/*
+import java.util.Scanner;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your password");
+        int count=0;
+        int pass = sc.nextInt();
+        do{
+            if(pass==0){
+                System.out.println("welcome");
+                break;
+            }
+            System.out.println("try againg");
+            pass = sc.nextInt();
+            count++;
+        }while (pass<=5);
+        if(pass>5){
+            System.out.println("card is blocked");
+        }
+    }
+}
+ */
+
+/*
+public class WhileLoop {
+    public static void main(String[] args) {
+        int num=0;
+        do {
+            System.out.println(num);
+            num++;
+        }while (num<=10);
+    }
+}
+ */
+
+/*
+import java.util.Scanner;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter number");
+        int num = sc.nextInt();
+        int count=1;
+        do{
+            System.out.println(num*count);
+            count++;
+        }while (count<=10);
+    }
+}
+ */
+
+
