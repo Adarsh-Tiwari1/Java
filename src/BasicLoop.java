e64d09c7e997487b2dbe2afbf148040bf703a943
Adarsh Tiwari <153628277+Adarsh-Tiwari1@users.noreply.github.com>
2024-09-24 14:41:07 +0530

    basic loop

---
+++ src/BasicLoop.java
@@ -0,0 +1,31 @@
+/*
+public class BasicLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i <=100; i++){
+            System.out.print((char)(int)i+"\t");
+        }
+    }
+}
+ */
+
+/*
+public class BasicLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 19000; i++) {
+            char c = (char) i;
+            System.out.println("ASCII value of " + c + " is " + i);
+        }
+    }
+}
+ */
+
+/*
+public class BasicLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 100; i++) {
+            char c = (char) i;
+            System.out.println("ascii value " + c +" = " + i);
+        }
+    }
+}
+ */
